f58d86be9ba851bc097dc18500a692692120c8f4
amansoni <amansoni2041999@gmail.com>
2023-01-30 22:30:17 +0530

    performing algorithm program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_program
+{
+    internal class Permutation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Algorithm Programs");
+            Console.WriteLine("1.Permutation ");
+            int Choice = Convert.ToInt32(Console.ReadLine());
+            switch (Choice)
+            {
+                case 1:
+                    Permutation permutation = new Permutation();
+                    String str = "ABC";
+                    int n = str.Length;
+                    permutation.permute(str, 0, n - 1);
+                    break;
+            }
+    }
+}
